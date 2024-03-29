f865506bf76edd76cba95223fee9e1f5e84c0c0e
Ramlakhan Gupta <136694457+ramlakhangupta@users.noreply.github.com>
2024-03-29 08:01:21 +0530

    Add files via upload

---
+++ Question Practise/array/partitionarray.java
@@ -0,0 +1,30 @@
+public class partitionarray {
+    public static void main(String[] args) {
+        int[] nums = {9,12,5,10,14,3,10};
+        System.out.println(pivotArray(nums,10));
+
+    }
+
+    public static  int[] pivotArray(int[] nums, int pivot) {
+        int[] ans = new int[nums.length];
+        int index = 0;
+        for (int num : nums) {
+            if (num < pivot) {
+                ans[index++] = num;
+            }
+        }
+        for (int num : nums) {
+            if (num == pivot) {
+                ans[index++] = num;
+            }
+        }
+        for (int num : nums) {
+            if (num > pivot) {
+                ans[index++] = num;
+            }
+        }
+
+        return ans;
+    }
+   
+}
