6a372f02ffe8d8860a0d76d0b43b122cf22ea5b8
Ramlakhan Gupta <136694457+ramlakhangupta@users.noreply.github.com>
2024-04-11 12:26:54 +0530

    Add files via upload

---
+++ Question Practise/recursion/KthBitInBinary.java
@@ -0,0 +1,15 @@
+public class KthBitInBinary {
+    public static void main(String[] args) {
+       System.out.println(findKthBit(4,11));
+    }
+    public static char findKthBit(int n, int k) {
+        if(n == 1 ) return '0';
+    
+        int l = (int) Math.pow(2,n) -1;
+        int mid = l/2 +1;
+        if(k == mid) return '1';
+        else if (k < mid ) return findKthBit(n-1, k);
+                else return findKthBit(n-1, l+1-k) == '0' ? '1' : '0';
+
+    }
+}
