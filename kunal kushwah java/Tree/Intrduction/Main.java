c0d9fdbeeb726c28f7a3ee72c2e7fdd4ba2cbfe9
Ramlakhan Gupta <136694457+ramlakhangupta@users.noreply.github.com>
2024-06-15 06:37:19 +0530

    Update Main.java

--- kunal kushwah java/Tree/Intrduction/Main.java
+++ kunal kushwah java/Tree/Intrduction/Main.java
@@ -1,10 +1,15 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        BinaryTree tree = new BinaryTree();
-        tree.popluate(scanner);
-        tree.prettyDisplay();
+        // Scanner scanner = new Scanner(System.in);
+        // BinaryTree tree = new BinaryTree();
+        // tree.popluate(scanner);
+        // tree.prettyDisplay();
+
+        BST tree =  new BST();
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+        tree.populatedSorted(nums);
+        tree.display();
     }
 }
