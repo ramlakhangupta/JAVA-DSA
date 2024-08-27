9ea003b18e8791adb0a77d7214360710bf12ff81
Ramlakhan Gupta <136694457+ramlakhangupta@users.noreply.github.com>
2024-08-27 11:46:06 +0530

    Add files via upload

--- PW/HashSet & HashMap/threeDigitEven.java
+++ PW/HashSet & HashMap/threeDigitEven.java
@@ -6,7 +6,8 @@ public class threeDigitEven {
     public int[] findEvenNumbers(int[] digits) {
         HashMap<Integer, Integer> map = new HashMap<>();
         
-        for (int i = 0; i < digits.length; i++) {
+        //Fill HashMap withgiven number
+        for (int i = 0; i < digits.length; i++) { 
             if (!map.containsKey(digits[i])) {
                 map.put(digits[i], 1);
             } else {
@@ -15,23 +16,24 @@ public class threeDigitEven {
             }
         }
 
+        //Find 3-digit numbers that contain even digits and add them to the ArrayList ans.
         ArrayList<Integer> ans = new ArrayList<Integer>();
 
-
-        for(int i = 100; i < 999; i+= 2) {
+        for(int i = 100; i < 999; i+= 2) { // check only for even digits so iterate with jump of two
             int x = i ;
             int c = x%10;
-            x = x/10;
+            x = x/10;       // by this we have to figure out the number of digits particularly
             int b = x%10;
             x = x/10;
             int a = x;
 
            
-
+            //CORE CONCEPT:
+            //ch   eck the a contains or not...
             if (map.containsKey(a)) {
-                int aFreq = map.get(a);
-                map.put(a, aFreq-1); 
-                    if (aFreq == 1 ) {
+                int aFreq = map.get(a); //by this we have to figure out the frequency is availabe or not in the map if yes then check for b but before, 
+                map.put(a, aFreq-1);    // we need to saw the frequency of key 
+                    if (aFreq == 1 ) {  // let for 222 we have to print only one, so we remove if the frequency is 1.
                         map.remove(a);
                     }
                 if (map.containsKey(b)) {
@@ -48,5 +50,13 @@ public class threeDigitEven {
                 map.put(a, aFreq);
             }
         }
+
+
+        int[] ans2 = new int[ans.size()];
+        for(int i = 0; i < ans2.length; i++) {
+            ans2[i] = ans.get(i);
+        }
+        return ans2;
+
     }
 }
