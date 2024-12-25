24bd75f776ed86b32b6877d1730c2e37e4e89d86
Ramlakhan Gupta <136694457+ramlakhangupta@users.noreply.github.com>
2024-12-25 07:29:04 +0530

    Create LeapOfFaith.java

---
+++ Code Story With Mik/Recursion/LeapOfFaith.java
@@ -0,0 +1,32 @@
+public class LeapOfFaith {
+
+    public static void main(String[] args) {
+        String s = "abc";
+        rever(s);
+    }
+
+    private static void rever(String s) {
+         solve(s, 0);
+    }
+
+    private static void solve(String s, int i) {
+        if (i>= s.length()) {
+            return;
+        }
+        solve(s, i+1);
+        System.out.print(s.charAt(i));
+        return;
+    }
+
+    public static int fac(int n ) {
+        if(n <=  1) {
+            return 1;
+
+        }
+
+        return n*fac(n-1);
+    }
+
+
+
+}
